ced38c79499bdd18f641bbe205f757392fe73548
WHM0304 <whm0304@naver.com>
2023-12-06 16:34:25 +0900

    Dto 만들기

--- Blackjack001/src/com/WHM0304/blackjack/Blackjack_001.java
+++ Blackjack001/src/com/WHM0304/blackjack/Blackjack_001.java
@@ -2,6 +2,8 @@ package com.WHM0304.blackjack;
 
 import java.util.Scanner;
 
+import com.WHM0304.blackjack.dto.CardDto;
+
 public class Blackjack_001 {
 	
 	public static void main(String[] args) {
@@ -9,8 +11,10 @@ public class Blackjack_001 {
 		Scanner scan = new Scanner(System.in);
 		int playerTotal = 0;
 		int dealerTotal = 0;
+		CardDto cardDto = new CardDto();
 		
 		System.out.println("블랙잭 게임");
+
 		
 		
 		
---
+++ Blackjack001/src/com/WHM0304/blackjack/dto/CardDto.java
@@ -0,0 +1,31 @@
+package com.WHM0304.blackjack.dto;
+
+import java.util.List;
+
+public class CardDto {
+	
+	 String Hearts;
+	 String Dias;
+	 String clobs;
+	 String spades;
+	private List<CardDto> cards = null;
+	public String num[] = {"A","2","3","4","5","6","7","8","9","J","Q","K"};
+	
+	
+	public static void Card() {
+		CardDto cardDto = new CardDto();
+		try {
+		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+	}
+	
+	
+	
+
+	
+
+}
