5e813f810f8840a24ccb18e14200eaadb9db39f4
WHM0304 <whm0304@naver.com>
2023-12-06 12:50:39 +0900

    개요

--- Blackjack001/src/com/WHM0304/blackjack/card/Deck.java
+++ Blackjack001/src/com/WHM0304/blackjack/card/Deck.java
@@ -1,23 +1,16 @@
 package com.WHM0304.blackjack.card;
 
-import java.util.List;
-
-
-
 public class Deck {
 	
-	private List<Card> cards;
+	public String Hearts;
+	public String Dias;
+	public String clobs;
+	public String spades;
 	
-	public Deck() {
-		String[] suits = {"Hearts" , "Diamonds" , "Clubs", "Spades"};
-		String[] ranks = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
-		for(String suit : suits) {
-			for(String rank : ranks) {
-				
-			}
-		}
-	}
-
+	public int deck;
+	
+	
+	
 	
 
 	
